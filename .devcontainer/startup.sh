82b2b2c61c04506e958cb62a9e6a2b71e9c92abd
Sylwia Budzynska <102833689+sylwia-budzynska@users.noreply.github.com>
2025-09-23 18:34:29 +0200

    Update devcontainer

---
+++ .devcontainer/startup.sh
@@ -0,0 +1,18 @@
+# Install CodeQL (bypassing SAML enforcement)
+# gh extension install github/gh-codeql
+
+URL=https://github.com/github/codeql-cli-binaries/releases
+LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' $URL/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+ZIP=$(mktemp codeql.XXXX.zip)
+
+echo Installing CodeQL version $LATEST_VERSION
+curl -fSqL -o $ZIP $URL/download/$LATEST_VERSION/codeql-linux64.zip
+unzip -q $ZIP -d /workspaces/ && rm $ZIP
+mkdir -p /home/vscode/.local/bin/
+# Set up a symlink to make CodeQL available in PATH
+# ln -s /workspaces/codeql/codeql /home/vscode/.local/bin/codeql
+
+echo Installed CodeQL version $LATEST_VERSION
+
+# Download submodules (if any)
+git submodule init && git submodule update --recursive --depth 1
